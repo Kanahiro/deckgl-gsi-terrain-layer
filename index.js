bf77e7e83c792c4cb29d2a4aa65d6c68d73db3e8
Kanahiro <mediterranean1769@gmail.com>
2020-11-07 21:47:21 +0900

    copyright and decsription

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Copyright (c) 2015 - 2017 Uber Technologies, Inc.
+// Copyright (c) 2020 Kanahiro Iguchi
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
